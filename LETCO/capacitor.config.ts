f1dfd8c8bd63d5a146bfac666887448161ab37ea
Noubissie Pavel <noubissiepavel@gmail.com>
2024-03-07 20:31:18 +0100

    init web ionic

---
+++ LETCO/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'LETCO',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
